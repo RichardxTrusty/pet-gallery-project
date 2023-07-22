5e5e87a96d71499c4e91db619fef1642dafff9ec
RichardXTrusty <ratrusty92@gmail.com>
2023-07-22 13:32:06 -0400

    Added Axios API

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,55 +1,55 @@
-// const images = [
-//   {
-//     url: "./assets/images/Photo-gallery-1.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-2.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-3.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-4.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-5.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
+const images = [
+  {
+    url: "./assets/images/Photo-gallery-1.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-2.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-3.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-4.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-5.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
 
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
-//   {
-//     url: "./assets/images/Photo-gallery-6.jpg",
-//   },
-// ];
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
+  {
+    url: "./assets/images/Photo-gallery-6.jpg",
+  },
+];
 
-const URL = "https://api.thedogapi.com/v1/images/search?limit=10 GET";
-function addGallery() {
-  axios.get(URL + "XuF0yCht6/?api_key=" + apiKey).then((response) => {
-    console.log(response);
-    displayGallery(response.data);
-  });
-}
+// const URL = "https://api.thedogapi.com/v1/images/search?limit=10 GET";
+// function addGallery() {
+//   axios.get(URL + "XuF0yCht6/?api_key=" + apiKey).then((response) => {
+//     console.log(response);
+//     displayGallery(response.data);
+//   });
+// }
 
-const URL = "https://api.thecatapi.com/v1/images/search?limit=10%20GET";
-function addGallery() {
-  axios.get(URL + "62f/?api_key=" + apiKey).then((response) => {
-    console.log(response);
-    displayGallery(response.data);
-  });
-}
+// const URL = "https://api.thecatapi.com/v1/images/search?limit=10%20GET";
+// function addGallery() {
+//   axios.get(URL + "62f/?api_key=" + apiKey).then((response) => {
+//     console.log(response);
+//     displayGallery(response.data);
+//   });
+// }
 
 const showGallery = () => {
   images.forEach((image) => {
@@ -82,8 +82,35 @@ const createImageContainer = (imgUrl) => {
   parentEl.appendChild(photoGalleryItem);
 };
 
+// const getPetImages = () => {
+// Make API Call using Axios
+//   axios
+//     .get("https://api.thedogapi.com/v1/images/search?limit=10 GET")
+//     .then((response) => {
+//       // store date images 
+//       images = response.data;
+//       // Then call showGallery
+//       showGallery();
+//     })
+//     .catch((e) => console.error(e));
+// };
+axios
+  .all([
+    axios.get("https://api.thedogapi.com/v1/images/search?limit=10 GET"),
+    axios.get("https://api.thecatapi.com/v1/images/search?limit=10 GET"),
+  ])
+  .then((response) => {
+    images = response[0].data;
+    images = images.concat(response[1].data);
+    // Then call showGallery();
+    showGallery();
+  })
+  .catch((e) => console.error(e));
+
 // Onload show gallery
-showGallery();
+// Get all the images from teh api
+// Then call showGallery()
+getPetImages();
 
 // Challenge
 // Start working on a new branch. Create a new branch feature/add-axios-api-integration. Please note we should not work on main branch
